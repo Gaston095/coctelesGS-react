959c52991f5072ce088c4bf4e319fd5b22c6a8a5
Gaston095 <Gastonsalas095@gmail.com>
2025-01-27 11:38:40 -0300

    Se realiza llamado a la api de cocteles para listar por primera letra

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import useFetch from "./useFetch";
 
 function App() {
-
+  const { data, loading, error } = useFetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a")
+  
   return (
     <>
       <h1>Hola Mundo!</h1>
+      <ul>
+        {error && <li>Error: {error}</li>}
+        {loading && <li>Loading...</li>}
+        {data?.map((drink)=>(<li key={drink.idDrink}>{drink.strDrink}</li>))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
