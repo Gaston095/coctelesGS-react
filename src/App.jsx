2e6ffe216cf32495a945f862e8bda67e8824f8a8
Gaston095 <Gastonsalas095@gmail.com>
2025-01-28 10:46:20 -0300

    Se agrega y configura Tailwind CSS al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import useFetch from "./useFetch";
 function App() {
   const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
-  const [letter, setLetter] = useState("a")
-  const { data, loading, error } = useFetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`)
-  
+  const [letter, setLetter] = useState("a");
+  const { data, loading, error } = useFetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`
+  );
+
   return (
     <>
       <h1>Hola Mundo!</h1>
@@ -22,10 +24,11 @@ function App() {
       <ul>
         {error && <li>Error: {error}</li>}
         {loading && <li>Loading...</li>}
-        {(data === null)? <h1>Vacio</h1> 
-        :
-        data?.map((drink)=>(<li key={drink.idDrink}>{drink.strDrink}</li>))
-}
+        {data === null ? (
+          <h1>Vacio</h1>
+        ) : (
+          data?.map((drink) => <li key={drink.idDrink}>{drink.strDrink}</li>)
+        )}
       </ul>
     </>
   );
