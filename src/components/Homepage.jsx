4663f089b3ca06d7fe2e77fa75429a7bf79d4c41
Gaston095 <Gastonsalas095@gmail.com>
2025-01-29 12:56:32 -0300

    Se modifica el homepage con una imagen de fondo

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,18 +1,21 @@
-import imgHomepage from "../assets/imgHomepage.jpg";
+import imgHomepage from "../assets/imgHomepage1.jpg";
 
 export default function Homepage() {
   return (
     <>
-      <div className="bg-black h-[100vh] text-white flex flex-col items-center pt-10">
-        <h1 className="text-center text-2xl font-bold p-2">
-          Bienvenido a Cocteles GS
-        </h1>
+      <div
+        className="bg-cover bg-center h-screen text-white flex flex-col items-center pt-10"
+        style={{ backgroundImage: `url(${imgHomepage})` }}
+      >
+        <div className="absolute inset-0 bg-black opacity-90"></div>
 
-        <img src={imgHomepage} className="w-[40%]" />
-        <p className="p-2 text-center mt-6">
-          A continuación encontraras simples y refrescantes tragos para
-          compartir con amigos y/o familia.
-        </p>
+        <div className="relative flex flex-col items-center justify-center h-full text-white font-bold">
+          <h1 className="text-3xl p-4">Bienvenido a Cocteles GS</h1>
+          <p className="text-center p-4">
+            A continuación encontraras simples y refrescantes tragos para
+            compartir con amigos y/o familia.
+          </p>
+        </div>
       </div>
     </>
   );
