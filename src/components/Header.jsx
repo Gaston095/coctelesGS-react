d10a7c16801874a2238b4493dc5756099accc83d
Gaston095 <Gastonsalas095@gmail.com>
2025-01-30 10:32:10 -0300

    Se realizan ajustes de estilo en el homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 export default function Header() {
-    return (
-        <>
-            <h1 className="bg-black text-white p-1">Cocteles GS</h1>
-        </>
-    )
+  return (
+    <>
+      <div className="h-10 bg-black absolute z-10">
+        <h1 className="text-white p-1">Cocteles GS</h1>
+      </div>
+    </>
+  );
 }
