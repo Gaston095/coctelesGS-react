cc8ebf33840bdd40ed465425e3a7368ce388acca
Gaston095 <Gastonsalas095@gmail.com>
2025-01-30 13:49:08 -0300

    Se cambia el diseño de la homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
     <>
-      <div className="h-10 bg-black absolute z-10">
-        <h1 className="text-white p-1">Cocteles GS</h1>
+      <div className="h-16 w-full bg-black absolute z-10 flex items-center">
+        <h1 className="text-white text-xl font-bold font-montserrat p-4">Cocteles GS</h1>
       </div>
     </>
   );
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,17 +1,16 @@
-import imgHomepage from "../assets/imgHomepage1.jpg";
+import imgHomepage from "../assets/imgHomepage19.jpg";
 
 export default function Homepage() {
   return (
     <>
       <div
-        className="bg-cover bg-center h-screen text-white flex flex-col items-center pt-10"
+        className="bg-black bg-contain bg-no-repeat h-screen text-white flex flex-col items-center pt-10"
         style={{ backgroundImage: `url(${imgHomepage})` }}
       >
-        <div className="absolute inset-0 bg-black opacity-90"></div>
-
-        <div className="relative flex flex-col items-center justify-center h-full text-white font-bold">
-          <h1 className="text-3xl p-4">Bienvenido a Cocteles GS</h1>
-          <p className="text-center p-4">
+        {/* <div className="absolute inset-0 bg-black opacity-50"></div> */}
+        <div className="relative mt-30 flex flex-col items-center h-full text-white font-bold ">
+          <h1 className="text-5xl p-4 font-montserrat">Bienvenido a Cocteles GS</h1>
+          <p className="text-center text-[17px] text-[#666] p-4 font-poppins max-w-md">
             A continuación encontraras simples y refrescantes tragos para
             compartir con amigos y/o familia.
           </p>
