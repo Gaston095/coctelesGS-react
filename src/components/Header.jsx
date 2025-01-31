15cd84ef706ada3f6d0e8e49b7fac5253f750644
Gaston095 <Gastonsalas095@gmail.com>
2025-01-31 11:13:53 -0300

    Se agrega el componente SearchBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import CoctailIcon from "../icons/coctailIcon";
+import SearchBar from "./SearchBar";
 
 
 export default function Header() {
   return (
     <>
-      <div className="h-16 w-full bg-black absolute z-10 flex items-center p-4">
+      <div className="h-16 w-full bg-black absolute z-10 flex justify-between items-center p-4">
         <h1 className="text-white text-xl font-bold font-montserrat flex items-center">
           <CoctailIcon/>
           Cocteles GS
         </h1>
+        <SearchBar />
       </div>
     </>
   );
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,7 @@
+export default function SearchBar() {
+    return (
+        <> 
+            <input className="text-white text-[14px] p-2 border-1 rounded-[10px] border-b-gray-500 outline-none" type="text" placeholder="buscar"/>
+        </>
+    )
+}
