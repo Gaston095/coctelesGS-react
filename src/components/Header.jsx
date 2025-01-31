935322b5d3e0ee770f32539be8ba1ce97d4a4d3f
Gaston095 <Gastonsalas095@gmail.com>
2025-01-31 10:35:54 -0300

    Se agrega icono a el logo del Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
+import CoctailIcon from "../icons/coctailIcon";
+
+
 export default function Header() {
   return (
     <>
-      <div className="h-16 w-full bg-black absolute z-10 flex items-center">
-        <h1 className="text-white text-xl font-bold font-montserrat p-4">Cocteles GS</h1>
+      <div className="h-16 w-full bg-black absolute z-10 flex items-center p-4">
+        <h1 className="text-white text-xl font-bold font-montserrat flex items-center">
+          <CoctailIcon/>
+          Cocteles GS
+        </h1>
       </div>
     </>
   );
