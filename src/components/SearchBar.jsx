a0e2640e72b121671ffa92086c156b79625b50e2
Gaston095 <Gastonsalas095@gmail.com>
2025-02-05 11:29:52 -0300

    Se agregan estilos a la SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,15 @@
+import SearchIcon from '../icons/SearchIcon'
+
 export default function SearchBar() {
     return (
-        <> 
-            <input className="text-white text-[14px] p-2 border-1 rounded-[10px] border-b-gray-500 outline-none" type="text" placeholder="buscar"/>
-        </>
+        <form className='w-[250px] relative'>
+            <div className="relative">
+                <input type='search' placeholder='Buscar..' className='focus:border-1 focus:border-slate-400 text-white w-full p-2 px-3 rounded-full text-[14px] bg-slate-800 placeholder:text-amber-100 outline-none'/>
+                <button className='absolute right-1 top-1/2 -translate-y-1/2 p-3 bg-slate-900 rounded-full cursor-pointer'>
+                    <SearchIcon />
+                </button>
+            </div>
+
+        </form>
     )
 }
